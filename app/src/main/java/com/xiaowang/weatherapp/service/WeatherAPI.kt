a2d30463bb4046d498b54c5b5b74e9d2cd2a26b2
epines1911 <70340462+epines1911@users.noreply.github.com>
2021-06-29 01:53:41 +0800

    set unit as metric

--- app/src/main/java/com/xiaowang/weatherapp/service/WeatherAPI.kt
+++ app/src/main/java/com/xiaowang/weatherapp/service/WeatherAPI.kt
@@ -9,6 +9,6 @@ interface WeatherAPI {
 
 //    http://api.openweathermap.org/data/2.5/weather?q=beijing&APPID=9b2b130f98fcf250cf5dcd557ce35ec5
 
-    @GET("data/2.5/weather?&APPID=9b2b130f98fcf250cf5dcd557ce35ec5")
+    @GET("data/2.5/weather?&units=metric&APPID=9b2b130f98fcf250cf5dcd557ce35ec5")
     fun getData(@Query("q") cityName: String): Single<WeatherModel>
 }
